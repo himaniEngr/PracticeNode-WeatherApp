2133e6bdc25b03651c93c1e0005bfe9e176d6de1
himaniEngr <engr.himani@gmail.com>
2022-03-07 14:39:34 +0530

    handling errors

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const request=require('request')
 const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=42.3605,-71.0596&units=f'
 request({ url: url , json:true}, (error, response) => {
     // const data = JSON.parse(response.body)
-    console.log(response.body.current)
+    console.log(response.body.current.weather_descriptions[0]+ ' . Temperature outsdide is '+ response.body.current.temperature+'f. It feels like '+ response.body.current.feelslike +'f outside')
 })
 
 const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
 
 request({ url: geocodeURL, json: true }, (error, response) => {
-    const latitude = response.body.features[0].center[0]
-    const longitude = response.body.features[0].center[1]
-    console.log(latitude, longitude)
+    if(error){
+        console.log('Could not connect to geocode api')
+    }else if (!response.body.features){
+        console.log('could not fetch location')
+    }else{
+        const latitude = response.body.features[0].center[0]
+        const longitude = response.body.features[0].center[1]
+        console.log(latitude, longitude)
+    }
+    
 })
